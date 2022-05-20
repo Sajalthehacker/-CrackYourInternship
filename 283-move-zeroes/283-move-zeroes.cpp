cdf8ddce22fedb1daf188b54558bc3dc15fa5af7
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2022-05-20 16:56:11 +0530

    Time: 26 ms (74.42%), Space: 19.2 MB (58.55%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -4,16 +4,13 @@ public:
         int i=0, j=0, cnt = 0;
         int n = nums.size();
         
-        while(i<n && nums[i] != 0) i++;
-        j = i;
-        
+        // Application of QuickSelect Algorithm        
         while(j<n){
             if(nums[j] != 0){
-                nums[i++] = nums[j];
+                swap(nums[i++] , nums[j]);
             }
             else cnt++;
             j++;
         }
-        for(int k=n-1; k>=n-cnt; k--) nums[k] = 0;
     }
 };
