f8da403c714860dc16c25a59edd9c35887e50f44
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2022-05-24 18:52:12 +0530

    Time: 3 ms (88.93%), Space: 6.9 MB (81.53%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -1,27 +1,51 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        stack<int> st;
-        st.push(-1);
-        
-        int len , maxi=0;
-        
-        for(int i=0; i<s.length(); i++){
-            char c = s[i];
+        //Solution with O(1) Space Complexity
             
-            if(c == '('){
-                st.push(i);
-            }
-            else{
-                st.pop();
-                if(st.empty()){
-                    st.push(i);
-                }else{
-                    len = i - st.top();
-                    maxi = max(maxi, len); 
-                }
+        int open = 0, close = 0;
+        int n = s.length();
+        int maxi = 0;
+        
+        // From left to right
+        for(int i=0; i<n; i++){
+            char c = s[i];
+            // Maintain the count of open and close brackets
+            if(s[i] == '(') open++;
+            else close++;
+            
+            // Assumption
+            // anytime close brackets shouldn't be greater than open brackets
+            
+            if(open == close){
+                int len = open + close;
+                maxi = max(maxi, len);
+            }else if(open < close){
+                open = close = 0;
             }
         }
+        open = 0;
+        close = 0;
+
+        
+        // From right to left
+        for(int i=n-1; i>=0; i--){
+            char c = s[i];
+            // Maintain the count of open and close brackets
+            if(s[i] == '(') open++;
+            else close++;
+            
+            // Assumption
+            // anytime close brackets shouldn't be greater than open brackets
+            
+            if(open == close){
+                int len = open + close;
+                maxi = max(maxi, len);
+            }else if(close < open){
+                open = close = 0;
+            }
+        }
+        
         return maxi;
     }
 };
