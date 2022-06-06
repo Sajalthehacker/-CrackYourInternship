8d6194995a7b598893fff02c93c8ab070e56799f
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2022-06-06 19:44:46 +0530

    Time: 157 ms (56.44%), Space: 64.8 MB (20.20%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.cpp
+++ 938-range-sum-of-bst/938-range-sum-of-bst.cpp
@@ -15,12 +15,12 @@ private:
         // Base Case
         if(!root) return;
         
-        
+        // Explore left of node, Only if the val id greater than low, 
+        //else all those will be smaller than val and there'll be no use in traversing them 
         if(root->val > low) dfs(root->left, low, high, sum);
-        if(root->val >= low && root->val <= high) {
-            cout<<root->val<<" ";
+        if(root->val >= low && root->val <= high) 
             sum += root->val;
-        }
+        // Only explore the right if val is less than high
         if(root->val < high) dfs(root->right, low, high, sum);
     }
 public:
