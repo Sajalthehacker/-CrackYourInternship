fd06963e5dcf2672721d10962c51a3eba01edf13
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2022-06-16 11:48:02 +0530

    Time: 256 ms (44.69%), Space: 231.7 MB (16.94%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,30 +1,25 @@
 class Solution {
+private:
+    void checkPalindromes(int l, int r, int n, int &maxL, int &ind1, int &ind2, string s){
+        while(l>=0 && r<n && s[l]==s[r]){
+            if(r-l+1 > maxL){
+                maxL = r-l+1;
+                ind1 = l;
+                ind2 = r;
+            }
+            r++; l--;
+        }
+    }
 public:
     string longestPalindrome(string s) {
         int n = s.length();
         int ind1 = -1, ind2 = -1, maxL = 0;
         for(int i=0; i<n; i++){
             // Check for Odd Length Palindrome
-            int l=i, r=i;
-            while(l>=0 && r<n && s[l]==s[r]){
-                if(r-l+1 > maxL){
-                    maxL = r-l+1;
-                    ind1 = l;
-                    ind2 = r;
-                }
-                r++; l--;
-            }
+            checkPalindromes(i, i, n, maxL, ind1, ind2, s);
             
             // Check for Even Length Palindrome
-            l=i, r=i+1;
-            while(l>=0 && r<n && s[l]==s[r]){
-                if(r-l+1 > maxL){
-                    maxL = r-l+1;
-                    ind1 = l;
-                    ind2 = r;
-                }
-                r++; l--;
-            }
+            checkPalindromes(i, i+1, n, maxL, ind1, ind2, s);
         }
         string ans = s.substr(ind1, ind2-ind1+1);
         return ans;
