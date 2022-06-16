b1717c583c3ed86d57fb139084c0f74ef5618557
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2022-06-16 15:23:43 +0530

    Time: 130 ms (96.22%), Space: 93.4 MB (12.27%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -4,16 +4,15 @@ public:
 
         // Kadane's Algo - Application
         // Same as Max-SubArray Sum, here we consider differences.
-        // link : https://bit.ly/3PzHIDG
         
         int n = prices.size();
-        int o_max = 0, curr_max=0;
+        int overallMax = 0, currMax=0;
         
         for(int i=1; i<n; i++){
-            curr_max = max(0, curr_max += prices[i]-prices[i-1]);
-            o_max = max(o_max, curr_max);
+            currMax = max(0, currMax += prices[i]-prices[i-1]);
+            overallMax = max(overallMax, currMax);
         }
         
-        return o_max;
+        return overallMax;
     }
 };
