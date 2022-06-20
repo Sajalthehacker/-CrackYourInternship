ce7f8be2563f0ab5806bed11d218abb19c194e9a
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2022-06-20 21:27:47 +0530

    Time: 242 ms (13.12%), Space: 96.5 MB (33.03%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.cpp
+++ 463-island-perimeter/463-island-perimeter.cpp
@@ -1,37 +1,34 @@
 class Solution {
 private:
-    int n, m;
+    int n, m, waterCnt=0;
     int directions[4][2] = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
     
-    bool isValid(int i, int j, vector<vector<int>>& grid){
-        return i>=0 && j>=0 && i<n && j<m && grid[i][j]==1;
-    }
-    
-    int checkBoundaries(int i, int j, vector<vector<int>>& grid){
-        int cnt = 0;
+     void dfs(int i, int j, vector<vector<int>>& grid){
+        if(i<0 || j<0 || i>=n || j>=m || grid[i][j] == 0){
+            waterCnt++;
+            return;
+        } 
+        if(grid[i][j] == -1) return;
+
+        grid[i][j] = -1; // If we arent allowed to modify grid, visited array can be used.
         for(auto it : directions){
-            if(isValid(i+it[0], j+it[1], grid)) cnt++;
+            dfs(i+it[0], j+it[1], grid);
         }
-        return cnt;
     }
-    
-    
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
         n = grid.size();
         m = grid[0].size();
         
-        int perimeter = 0;
-        
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(grid[i][j] == 1){
-                    int oneCount = checkBoundaries(i, j, grid);
-                    perimeter += (4-oneCount);
+                    dfs(i, j, grid);
+                    break;
                 }
             }
         }
-        return perimeter;
+        return waterCnt;
 
     }
 };
