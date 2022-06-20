4a1bf18c9e9aa89c921a8c4b3d7492f779128c1c
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2022-06-20 13:09:35 +0530

    Time: 14 ms (54.96%), Space: 10.4 MB (55.24%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.cpp
+++ 841-keys-and-rooms/841-keys-and-rooms.cpp
@@ -1,6 +1,8 @@
 class Solution {
 private:
+    int roomCnt = 0;
     void dfs(int source, vector<bool> &vis, vector<vector<int>> &rooms){
+        roomCnt++;
         vis[source] = true;
         for(int it : rooms[source]){
             if(vis[it] == true) continue;
@@ -15,9 +17,7 @@ public:
         // After this dfs if there is a key(edge) to every room,
         // By the end every room should be visited
         // If there is any room left unvisited, then impossible
-        for(bool it : vis){
-            if(it == false) return false;
-        }
-        return true;
+        
+        return roomCnt == n;
     }
 };
