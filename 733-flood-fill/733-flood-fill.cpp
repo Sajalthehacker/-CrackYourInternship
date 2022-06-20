0352ab6bf351aa1e75e856c1e30c65f8e209bd47
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2022-06-20 22:27:56 +0530

    Time: 23 ms (12.81%), Space: 13.9 MB (91.19%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,27 +1,26 @@
 class Solution {
 private:
-    int n;
-    int m;
-    void dfs(int i, int j, int prevColor, int newColor, vector<vector<int>>& image){
-        if(i<0 || j<0 || i>=n || j>=m) return; // Out of Bounds
-        if(image[i][j] != prevColor) return; // only src color should be changed      
-        
-        image[i][j] = newColor;
-        
-        dfs(i, j+1, prevColor, newColor, image);
-        dfs(i, j-1, prevColor, newColor, image);
-        dfs(i+1, j, prevColor, newColor, image);
-        dfs(i-1, j, prevColor, newColor, image);
-
+int n, m;
+int directions[4][2] = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
+    
+void dfs(int i, int j, int prevColor, int newColor, vector<vector<int>>& image){
+    if(i<0 || j<0 || i>=n || j>=m) return; // Out of Bounds
+    if(image[i][j] != prevColor) return; // only src color should be changed      
+    
+    image[i][j] = newColor; // cahnging the color to new Color
+    for(auto it : directions){
+        dfs(i+it[0], j+it[1], prevColor, newColor, image);
     }
+
+}
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
         n = image.size();
         m = image[0].size();
         
-        if(image[sr][sc] != newColor)
+        if(image[sr][sc] != newColor) // We only do dfs, if we have to change the Color
             dfs(sr, sc, image[sr][sc], newColor, image);
-        
+
         return image;
     }
 };
