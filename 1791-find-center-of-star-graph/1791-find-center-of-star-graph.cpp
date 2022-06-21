aa72565be3062305ec3276810adf1d09c59b7372
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2022-06-22 00:13:52 +0530

    Time: 457 ms (9.75%), Space: 67.4 MB (50.94%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -6,7 +6,7 @@ public:
         // So it would be any of the node in first pair
         
         if(edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1]){
-            // here e[0][0] is common so this is Star Node
+            // here edges[0][0] is common so this is Star Node
             return edges[0][0];
         }
         else return edges[0][1];
