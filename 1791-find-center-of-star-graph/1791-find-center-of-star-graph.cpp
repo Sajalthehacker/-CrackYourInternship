62d6118298e67ad57c697e36b0d387a1f091614e
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2022-06-22 00:13:43 +0530

    Time: 294 ms (48.46%), Space: 67.2 MB (81.88%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
-        int n = edges.size()+1;
-        vector<int> degree(n+1, 0);
+        // Observation :
+        // Star node should be present in every edge,
+        // So it would be any of the node in first pair
         
-        int posNode = -1; //for storing nodes with degree 2
-        
-        for(auto it : edges){
-            degree[it[0]]++;
-            degree[it[1]]++;
-            
-            if(degree[it[0]] >= 3) return it[0]; // We found Star Node, which have degree 3
-            else if(degree[it[0]] == 2) posNode = it[0]; // If not 3, starnode's degree ll be 2
-            
-            if(degree[it[1]] >= 3) return it[1];
-            else if(degree[it[1]] == 2) posNode = it[1];
+        if(edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1]){
+            // here e[0][0] is common so this is Star Node
+            return edges[0][0];
         }
-
-        return posNode;
+        else return edges[0][1];
     }
 };
