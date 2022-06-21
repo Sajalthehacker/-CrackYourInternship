bd5d80db18e726736bb702fcb31c28c20c9717f6
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2022-06-22 01:44:41 +0530

    Time: 373 ms (72.15%), Space: 105.7 MB (52.79%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
+        
+        // Here the ans would be all the nodes, without incoming edges
+        // Observation :
+        // All the nodes having incoming edge, can be visited from a souce node
+        // And that doesn't have incoming edge
+        
         vector<bool> vis(n, false);
         for(auto it : edges){
             vis[it[1]] = true;
