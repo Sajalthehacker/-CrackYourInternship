9f42d09b0dedb37cd235a889a7cd5bdd2b636058
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2022-06-23 14:11:53 +0530

    Time: 26 ms (74.17%), Space: 14.1 MB (45.89%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,32 @@
+class Solution {
+private:
+    bool dfs(int source, vector<bool> &vis, vector<bool>& check, vector<int> adj[]){
+        vis[source] = true;
+        check[source] = true;
+        
+        for(int it: adj[source]){
+            if(check[it] == true) return true;
+            if(vis[it] == false){
+                if(dfs(it, vis, check, adj)) return true;
+            }
+        }
+        check[source] = false;
+        return false;
+    }
+public:
+    bool canFinish(int n, vector<vector<int>>& prereq) {
+        vector<int> adj[n];
+        vector<bool> vis(n, false);
+        vector<bool> check(n, false);
+
+        for(auto it : prereq){
+            adj[it[0]].push_back(it[1]);
+        }
+        
+        for(int i=0;i<n;i++){
+            if(dfs(i, vis, check, adj))
+                return false;
+        }
+        return true;
+    }
+};
