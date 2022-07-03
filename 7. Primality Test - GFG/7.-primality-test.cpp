b501dd44311dc0cd1215a73400154823c748b25f
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2022-07-03 10:53:37 +0530

    Added solution - LeetHub

---
+++ 7. Primality Test - GFG/7.-primality-test.cpp
@@ -0,0 +1,44 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    bool isPrime(int N){
+        if(N == 2) return true;
+        if(N < 2) return false;
+        for(int i=2; i*i <= N; i++){
+            if(N%i == 0) return false;
+        }
+        return true;
+    }
+
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int T; //testcases
+    cin>>T; //input testcases
+    while(T--) //while testcase have not been exhausted
+    {
+        int N;
+        cin>>N; //input n
+        Solution ob;
+        if(ob.isPrime(N)) 
+            cout<<"Yes";
+        else
+            cout<<"No";
+            
+        cout << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
