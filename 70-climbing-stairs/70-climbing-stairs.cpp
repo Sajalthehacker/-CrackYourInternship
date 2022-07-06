6dcee2d0cecd31e5b0e980d297157dcc5ced2d12
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2022-07-06 23:07:40 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (56.37%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int climbStairs(int n) {
         // Tabulation
+        // This is memory optimised version of tabulation method
+        // please look into previous submissions
         if(n <= 2) return n;
         
         int b = 1, a = 1, res;
