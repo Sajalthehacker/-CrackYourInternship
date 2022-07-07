cac0af91bd76e975ec9602c9505c173a7fa57391
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2022-07-07 22:41:38 +0530

    Added solution - LeetHub

---
+++ 20. Strongly Connected Components (Kosaraju's Algo) - GFG/20.-strongly-connected-components-kosarajus-algo.cpp
@@ -0,0 +1,98 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    void dfs(vector<bool>& visited, vector<int> adj[], int x, stack<int>& st){
+        visited[x]=true;
+        for(int i = 0 ; i < adj[x].size() ; i++){
+            if(!visited[adj[x][i]])
+            { 
+                dfs(visited, adj, adj[x][i], st);   
+            }
+        }
+        st.push(x);
+    }
+    void dfs2(vector<bool>& visited, vector<vector<int>>& adj, int x){
+        visited[x]=true;
+        for(int i = 0 ; i < adj[x].size() ; i++)
+        {
+            if(!visited[adj[x][i]]){                 
+                dfs2(visited, adj, adj[x][i]);       
+            }
+        }
+    }
+    int kosaraju(int V, vector<int> adj[])
+    {
+        stack<int> st;
+        vector<bool> visited(V, false);
+        for(int i = 0 ; i < V; i++){
+            if(!visited[i]){
+                dfs(visited, adj, i, st);
+            }
+        }
+  
+        vector<int> order;
+        while(!st.empty()){
+            order.push_back(st.top());
+            st.pop();
+        }
+        vector<vector<int>> trans;
+        for(int i = 0 ; i < V ; i++){
+            vector<int> temp;
+            temp.push_back(i);
+            trans.push_back(temp);
+        }
+        for(int i = 0 ; i < V ; i++){
+            for(int j = 0 ; j < adj[i].size() ; j++){
+                trans[adj[i][j]].push_back(i);
+            }
+        }
+
+        int cnt=0;
+        vector<bool> visit(V, false);
+        for(int i = 0 ; i < V; i++){
+            if(!visit[order[i]]){
+                dfs2(visit, trans, order[i]);
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
+
+// { Driver Code Starts.
+
+
+int main()
+{
+    
+    int t;
+    cin >> t;
+    while(t--)
+    {
+    	int V, E;
+    	cin >> V >> E;
+
+    	vector<int> adj[V];
+
+    	for(int i = 0; i < E; i++)
+    	{
+    		int u, v;
+    		cin >> u >> v;
+    		adj[u].push_back(v);
+    	}
+
+    	Solution obj;
+    	cout << obj.kosaraju(V, adj) << "\n";
+    }
+
+    return 0;
+}
+
+  // } Driver Code Ends
