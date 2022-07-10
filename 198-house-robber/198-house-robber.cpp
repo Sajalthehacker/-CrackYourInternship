600ff0335ad72303d780fdef57ca22d5de46cb9f
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2022-07-10 13:29:52 +0530

    Time: 3 ms (54.11%), Space: 7.8 MB (47.13%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        
         // this sub is memory optimised visit prev sub if confused
-        
         int n = nums.size();
         if(n == 1) return nums[0];
         
-        int prev1 = nums[0];
-        int prev2 = 0;
-        int ans;
+        int prev1 = nums[0], prev2 = 0, ans;
         
         for(int i=1; i<n; i++){
             int take = nums[i];
             if(i > 1) take += prev2;
-
+            
             int not_take = prev1;
             
             ans = max(take, not_take);
