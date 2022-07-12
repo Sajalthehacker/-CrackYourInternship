6b3a14828a5712d876d1169cbb64e6ae3c3a69b8
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2022-07-12 18:16:09 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (73.50%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
+        // Can be done by DP also in TC : O(n*m), SC : O(n*m); refer prev subs
+        // this is a combinatorics approach in TC : O(min(n, m)), SC : O(1)
         int total = m + n - 2;
         int r = min(n, m) - 1;
         double res = 1;
