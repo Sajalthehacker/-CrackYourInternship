660b7975ab3c522f0ed1b565198726cea79481a3
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2022-07-14 12:06:37 +0530

    Time: 12 ms (71.22%), Space: 10.1 MB (28.77%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -2,7 +2,6 @@ class Solution {
 private:
     int fn(int i, int j, vector<vector<int>> &dp, vector<vector<int>>& grid){
         if(i==0 && j==0) return grid[0][0];
-
         if(j == 0) return grid[i][j] + fn(i-1, j, dp, grid);
         if(i == 0) return grid[i][j] + fn(i, j-1, dp, grid);
         
@@ -12,7 +11,8 @@ private:
     }
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        
+        // Recursive Approach 
+        // TC : O(n*m) && SC : O(n*m) + O(n+m-2)-> extra space for recursion call stack
         int n = grid.size(), m = grid[0].size();
         vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
         return fn(n-1, m-1, dp, grid);
