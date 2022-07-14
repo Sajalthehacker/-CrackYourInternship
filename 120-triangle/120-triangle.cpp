6fd1ce52dc91519fab18e65ced7414d9edafd765
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2022-07-14 15:28:24 +0530

    Time: 12 ms (35.42%), Space: 9 MB (36.59%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,19 +1,16 @@
 class Solution {
-private:
-    int fn(int i, int j, int &n, vector<vector<int>>& dp, vector<vector<int>>& tri){
-        if(i >= n) return 0;
-        if(i == n-1) return tri[i][j];
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        return dp[i][j] = tri[i][j] + min(fn(i+1, j, n, dp, tri) , fn(i+1, j+1, n, dp, tri));
-    }
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        // Rescursive Approach
-        // TC : O(n*(n-1)/2) ~ O(n^2)    &&   SC : O(n^2) + O(n)->extra space for Recursion Stack
+        // Iterative Approach
+        // TC : O(n^2)    &&   SC : O(n^2)
         
         int n = triangle.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        return fn(0, 0, n, dp, triangle);
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        for(int i=n-1; i>=0; i--){
+            for(int j=0; j<=i; j++){
+                dp[i][j] = triangle[i][j] + min(dp[i+1][j], dp[i+1][j+1]);
+            }
+        }
+        return dp[0][0];
     }
 };
