e1ff5c4d5fe063892d45347593fdc44097f7d4c6
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2022-07-14 12:15:06 +0530

    Time: 21 ms (21.63%), Space: 10 MB (52.10%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -10,8 +10,10 @@ public:
         dp[n-1][m-1] = grid[n-1][m-1];
         for(int i=n-1; i>=0; i--){
             for(int j=m-1; j>=0; j--){
-                if(i == n-1 && j == m-1) dp[i][j] = grid[i][j];
-                else if(i == n-1) dp[i][j] = grid[i][j] + dp[i][j+1];
+                if(i == n-1 && j == m-1) dp[i][j] = grid[i][j]; // Edgecase - already in final block
+                // In bottom line , we can only go right (i, j) -> (i, j+1)
+                else if(i == n-1) dp[i][j] = grid[i][j] + dp[i][j+1]; 
+                // In right-most column , we can only go down (i, j) -> (i+1, j)
                 else if(j == m-1) dp[i][j] = grid[i][j] + dp[i+1][j];
                 else{
                     dp[i][j] = grid[i][j] + min(dp[i+1][j], dp[i][j+1]);
