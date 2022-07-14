f500d5e8a520640f3edd269f82517d70bb7c79dc
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2022-07-14 12:29:00 +0530

    Time: 9 ms (57.59%), Space: 9 MB (36.59%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,16 +1,16 @@
 class Solution {
+private:
+    int fn(int i, int j, int &n, vector<vector<int>>& dp, vector<vector<int>>& tri){
+        if(i >= n) return 0;
+        if(i == n-1) return tri[i][j];
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        return dp[i][j] = tri[i][j] + min(fn(i+1, j, n, dp, tri) , fn(i+1, j+1, n, dp, tri));
+    }
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<int> dp = triangle[n-1];
-        // DP BottomsUp Approach
-        for(int i=n-2; i>=0; i--){
-            for(int j=0; j<=i; j++){
-                dp[j] = triangle[i][j] + min(dp[j], dp[j+1]);
-                // cout<<dp[j]<<" ";
-            }
-            // cout<<endl;
-        }
-        return dp[0];
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        return fn(0, 0, n, dp, triangle);
     }
 };
