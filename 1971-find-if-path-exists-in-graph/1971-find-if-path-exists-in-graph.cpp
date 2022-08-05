605fc7bc46ebe77e086d3be55f5bd44d09031585
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2022-08-05 14:17:41 +0530

    Time: 836 ms (75.71%), Space: 145.6 MB (91.50%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    bool validPath(int n, vector<vector<int>>& edges, int source, int dest) {
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        // Making Adjacency list
         vector<int> adj[n];
-        vector<bool> vis(n, false);
         for(int i=0; i<edges.size(); i++){
             adj[edges[i][0]].push_back(edges[i][1]);
             adj[edges[i][1]].push_back(edges[i][0]);
         }
+        vector<bool> vis(n, false);
+        vis[source] = true;
         
+        // Running a BFS to search the node
+        // Choosed BFS coz, there isn't extra stack space involved in BFS
         queue<int> que;
         que.push(source);
         while(!que.empty()){
             int node = que.front();
-            que.pop();
             vis[node] = true;
-            if(node == dest){
-                return true;
-            }
-            for(int it : adj[node]){
-                if(!vis[it]) que.push(it);
-            }
+            que.pop();
+            // Checking if pre node is the destination node
+            if(node == destination) return true;
+            for(int it : adj[node]) 
+                if(vis[it] == false) que.push(it);
         }
         return false;
-        
     }
 };
