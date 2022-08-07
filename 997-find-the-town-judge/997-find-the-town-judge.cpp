0370d9ab2801a907e96ebf34f9c99ed4df14398d
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2022-08-07 17:45:02 +0530

    Time: 430 ms (7.36%), Space: 68.9 MB (34.74%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.cpp
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
         vector<int> in(n, 0);
-        vector<int> out(n, 0);
         
         for(auto it : trust){
             // Person 'a' trusts Person 'b'
             int a = it[0], b = it[1];
             // Count incoming and outgoing edges of the Graph
-            in[b-1]++;
-            out[a-1]++;
+            in[a-1] = -1;
+            if(in[b-1] != -1) in[b-1]++;
+            
         }
         for(int i=0; i<n; i++){
-            if(out[i] == 0 && in[i] == n-1) return i+1;
+            if(in[i] == n-1) return i+1;
         }
         return -1;
 
