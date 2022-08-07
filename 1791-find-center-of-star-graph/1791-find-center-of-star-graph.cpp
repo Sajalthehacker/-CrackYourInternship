34eb564cbbc2ca13d117920e582a2da59d3c8501
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2022-08-07 17:25:11 +0530

    Time: 212 ms (92.87%), Space: 67.4 MB (56.45%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -1,14 +1,7 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
-        // Observation :
-        // Star node should be present in every edge,
-        // So it would be any of the node in first pair
-        
-        if(edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1]){
-            // here edges[0][0] is common so this is Star Node
-            return edges[0][0];
-        }
+        if(edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1]) return edges[0][0];
         else return edges[0][1];
     }
 };
