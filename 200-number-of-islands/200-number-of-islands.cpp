8dc9cbb3d319d89c897b2bb5f23c669975b07bd7
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2022-08-28 14:08:14 +0530

    Time: 55 ms (53.13%), Space: 12.5 MB (43.92%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,39 +1,26 @@
 class Solution {
 private:
-    int n, m, cnt = 0;
-    int directions[4][2] ={{-1, 0}, {0, 1}, {1, 0}, {0, -1}}; 
-    void dfs(int i, int j, vector<vector<bool>> &vis, vector<vector<char>> &grid){
-        
-        if(i<0 || j<0 || i>=n || j>=m) return; // Index out of bounds condition
-        if (grid[i][j] != '1' || vis[i][j]) return; // No Bridge Condition
-
+    void dfs(int i, int j, int n, int m, vector<vector<bool>>& vis, vector<vector<char>>& grid){
+        if(i<0 || j<0 || i>=n || j>=m || vis[i][j] || grid[i][j] == '0') return;
         vis[i][j] = true;
-        // cout << cnt << " ";
-        // Further dfs calls : up, down, right, left
-        // dfs(i+1, j, vis, grid); // dfs(i-1, j, vis, grid); 
-        // dfs(i, j+1, vis, grid); // dfs(i, j-1, vis, grid);
-        // These four manual calls can be written cleanly as direction array
-        for(auto it : directions){
-            dfs(i+it[0], j+it[1], vis, grid);
-        }
-
-    }
-    
+        dfs(i+1, j, n, m, vis, grid);
+        dfs(i-1, j, n, m, vis, grid);
+        dfs(i, j+1, n, m, vis, grid);
+        dfs(i, j-1, n, m, vis, grid);
+    } 
 public:
     int numIslands(vector<vector<char>>& grid) {
-        n = grid.size();
-        m = grid[0].size();
+        int n = grid.size(), m = grid[0].size();
         vector<vector<bool>> vis(n, vector<bool>(m, false));
-        
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(vis[i][j] == false && grid[i][j] == '1'){
-                    // increse the cnt and make a dfs call
-                    cnt++;
-                    dfs(i, j, vis, grid);
+        int islandCnt = 0;
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j < m; j++){
+                if(!vis[i][j] && grid[i][j] == '1'){
+                    islandCnt++;
+                    dfs(i, j, n, m, vis, grid);
                 }
             }
         }
-        return cnt;
+        return islandCnt;
     }
 };
