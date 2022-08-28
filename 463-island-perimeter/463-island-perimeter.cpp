f1b86a24baba59c761df62d048106d813e6a158c
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2022-08-28 14:18:21 +0530

    Time: 136 ms (81.59%), Space: 96.2 MB (60.50%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.cpp
+++ 463-island-perimeter/463-island-perimeter.cpp
@@ -1,34 +1,21 @@
 class Solution {
-private:
-    int n, m, waterCnt=0;
-    int directions[4][2] = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
-    
-     void dfs(int i, int j, vector<vector<int>>& grid){
-        if(i<0 || j<0 || i>=n || j>=m || grid[i][j] == 0){
-            waterCnt++;
-            return;
-        } 
-        if(grid[i][j] == -1) return;
-
-        grid[i][j] = -1; // If we arent allowed to modify grid, visited array can be used.
-        for(auto it : directions){
-            dfs(i+it[0], j+it[1], grid);
-        }
-    }
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
-        n = grid.size();
-        m = grid[0].size();
+        int n = grid.size(), m = grid[0].size();
+        int perimeter = 0;
         
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(grid[i][j] == 1){
-                    dfs(i, j, grid);
-                    break;
+                    perimeter += 4;
+                    if(i>0 && grid[i-1][j] == 1) perimeter--;
+                    if(i<n-1 && grid[i+1][j] == 1) perimeter--;
+                    if(j>0 && grid[i][j-1] == 1) perimeter--;
+                    if(j<m-1 && grid[i][j+1] == 1) perimeter--;
                 }
             }
         }
-        return waterCnt;
-
+        return perimeter;
+        
     }
 };
