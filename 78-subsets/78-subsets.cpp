978c128909da6e36fb95ca1e2a207a2bbc89d8e2
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2022-09-12 18:28:03 +0530

    Time: 4 ms (57.54%), Space: 7 MB (87.47%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,21 +1,24 @@
 class Solution {
 private:
-    void subsetSum(int ind, vector<int>& nums, vector<int>& ds, vector<vector<int>>& ans){
-        if(ind==nums.size()){
-            ans.push_back(ds);
+    void fn(int i, vector<int> &nums, vector<int> &ds, vector<vector<int>> &powerSet){
+        if(i == nums.size()){
+            powerSet.push_back(ds);
             return;
         }
-        ds.push_back(nums[ind]);
-        subsetSum(ind+1, nums, ds, ans);
+        ds.push_back(nums[i]);
+        fn(i+1, nums, ds, powerSet);
         ds.pop_back();
-        subsetSum(ind+1, nums, ds, ans);
+        fn(i+1, nums, ds, powerSet);
     }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
+        // As we need the elments in inc order, lets sort the array
         sort(nums.begin(), nums.end());
+        
+        vector<vector<int>> powerSet;
         vector<int> ds;
-        vector<vector<int>> ans;
-        subsetSum(0, nums, ds, ans);
-        return ans;
+        
+        fn(0, nums, ds, powerSet);
+        return powerSet;
     }
 };
