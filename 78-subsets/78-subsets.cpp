2b5d7fcd4debcf15cc4f419f70170238635f1901
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2022-09-12 18:29:13 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (96.05%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -6,13 +6,12 @@ private:
             return;
         }
         ds.push_back(nums[i]);
-        fn(i+1, nums, ds, powerSet);
+        fn(i+1, nums, ds, powerSet);  // Take Condition
         ds.pop_back();
-        fn(i+1, nums, ds, powerSet);
+        fn(i+1, nums, ds, powerSet);  // Not-Take Condition
     }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        
         vector<vector<int>> powerSet;
         vector<int> ds;
         
