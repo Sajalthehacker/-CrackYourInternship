193032f0bd0313eb5cab9bc5f46ed897b23d1296
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2022-09-28 18:23:19 +0530

    Time: 24 ms (57.41%), Space: 12.1 MB (50.88%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -3,7 +3,22 @@ private:
     vector<vector<int>> allPaths;
     int n;
     
-    void dfs(int vertex, vector<int> &prePath, vector<vector<int>>& adj, vector<bool> vis){
+    // void dfs(int vertex, vector<int> &prePath, vector<vector<int>>& adj, vector<bool> vis){
+    //     vis[vertex] = true;
+    //     prePath.push_back(vertex);
+    //     if(vertex == n-1){
+    //         allPaths.push_back(prePath);
+    //     }
+    //     for(int child : adj[vertex]){
+    //         if(vis[child]) continue;
+    //         dfs(child, prePath, adj, vis);
+    //     }
+    //     prePath.pop_back();
+    //     vis[vertex] = false;
+    // }
+    
+    
+    void dfs(int vertex, vector<int> &prePath, vector<vector<int>>& adj, vector<bool> &vis){
         vis[vertex] = true;
         prePath.push_back(vertex);
         if(vertex == n-1){
