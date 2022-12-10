408b5953994bac790682130388d51965e372b440
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2022-12-10 23:44:51 +0530

    Time: 323 ms (28.20%), Space: 77.3 MB (89.61%) - LeetHub

--- 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
@@ -15,15 +15,19 @@ private:
     int dfs(TreeNode* root){
         if(root == NULL) return 0;
         
+        // Idea : ans is max of all subTreeSum*(total - subTreeSum)
+        // we did post order because, in that case
+        // leafs are done first and root is added, which is indirectle subtree sum
+        
         long long currSum = dfs(root -> left) + dfs(root -> right) + root -> val;
         ans = max(ans, currSum*(fullSum - currSum));
         return currSum;
     }
 public:
     int maxProduct(TreeNode* root) {
-        fullSum = dfs(root);
+        fullSum = dfs(root); // It'll get the sum of toatal tree
 
-        dfs(root);
+        dfs(root); //  in the next traversal, we calculate sum of subrees
         return ans%mod;
     }
 };
