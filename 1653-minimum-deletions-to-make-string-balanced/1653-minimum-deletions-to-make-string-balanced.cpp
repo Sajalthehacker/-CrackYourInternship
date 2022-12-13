4b6cbc6e901973f88e0d4834225b43e47d9b5568
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2022-12-13 18:57:31 +0530

    Time: 122 ms (83.27%), Space: 22.1 MB (67.68%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -2,26 +2,20 @@ class Solution {
 public:
     int minimumDeletions(string s) {
         int n = s.size();
-        int preB[n];
-        int suffA[n];
+        int cntA = 0, cntB = 0;
+        for(char it : s) 
+            if(it == 'a') cntA++;
+        // if we delete all a's the final string is balances( i.e. "bbbb...b")
+        int ans = cntA; // dleteing all the a's 
         
-        if(s[0] == 'b') preB[0] = 1;
-        else preB[0] = 0;
-        if(s[n-1] == 'a') suffA[n-1] = 1;
-        else suffA[n-1] = 0;
-
-        for(int i=1; i<n; i++){
-            if(s[i] == 'b') preB[i] = preB[i-1] + 1;
-            else preB[i] = preB[i-1];
-            
-            if(s[n-i-1] == 'a') suffA[n-i-1] = suffA[n-i] + 1;
-            else suffA[n-i-1] = suffA[n-i];
+        // Now we are looking for a split where all the left b's are deleted 
+        // and all the right a's are deleted string end up as ("aaa...aabbb..bbb")
+        for(char it : s){
+            if(it == 'a') cntA--; // we arent deleting this paticular 'a'
+            else cntB++; // we are deleting 'b', coz we dont want any b sitting in left side
+            ans = min(ans, cntA+cntB);
         }
-        int ans = INT_MAX;
-        for(int i=0; i<n; i++){
-            if((preB[i] == 1 && suffA[i] == 0) || (preB[i] == 0 && suffA[i] == 1)) return 0;
-            ans = min(ans, preB[i] + suffA[i] - 1);
-        }
+        
         return ans;
     }
 };
