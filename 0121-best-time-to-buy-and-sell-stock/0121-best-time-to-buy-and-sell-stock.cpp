55bbf9c5bbca64446716b3ffa5cfd0b58ec83571
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2023-01-08 13:02:29 +0530

    Time: 124 ms (97.26%), Space: 93.3 MB (53.97%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int prevMin = prices[0], maxP = 0;
-        for(int it : prices){
-            if(it < prevMin) prevMin = it;
-            else maxP = max(maxP, it - prevMin);
+        // Kadane's Algo - Application
+        // Same as Max-SubArray Sum, here we consider differences.
+        // link : https://bit.ly/3PzHIDG
+        
+        int n = prices.size(), maxP = 0, currP = 0;
+        for(int i=1; i<n; i++){
+            currP = max(0, currP + (prices[i] - prices[i-1]));
+            maxP = max(maxP, currP);
         }
         return maxP;
     }
