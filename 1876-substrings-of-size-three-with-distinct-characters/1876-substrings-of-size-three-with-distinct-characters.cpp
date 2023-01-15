bc5a1cb12223bf959c78d8513875d1b52b99b619
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2023-01-15 17:19:50 +0530

    Time: 4 ms (55.40%), Space: 6.2 MB (49.62%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
@@ -3,6 +3,8 @@ public:
     int countGoodSubstrings(string s) {
         int n = s.size();
         int ans = 0;
+        // Brute Force, its done like this only because substring length is 3
+        // we can generalise the question for k length substruing using technique of sliding window
         for(int i=0; i<=n-3; i++){
             if(s[i] != s[i+1] && s[i+1] != s[i+2] && s[i] != s[i+2]) ans++;
         }
