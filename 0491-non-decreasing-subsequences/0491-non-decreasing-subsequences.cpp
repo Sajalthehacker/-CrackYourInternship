c3a59993b5ffc30939f718e44f65b20e2d45728b
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2023-01-20 11:52:14 +0530

    Time: 119 ms (38.75%), Space: 25.9 MB (41.87%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -2,27 +2,17 @@ class Solution {
 public:
     void fn(int ind, vector<int> &curr, vector<int> &nums, set<vector<int>> &stt){
         if(ind == nums.size()){
-            if(curr.size() >=2){
-                for(int i=1; i<curr.size(); i++)
-                    if(curr[i] < curr[i-1]) return;
-                stt.insert(curr); 
-            }
+            if(curr.size() >= 2) stt.insert(curr); 
             return;
         }
-        else if(curr.size() >= 2) {
-            bool flag = true;
-            for(int i=1; i<curr.size(); i++){
-                if(curr[i] < curr[i-1]) {
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag) stt.insert(curr);
-        }
+        if(curr.size() >= 2) 
+            stt.insert(curr);
         
-        curr.push_back(nums[ind]);
-        fn(ind+1, curr, nums, stt);
-        curr.pop_back();
+        if(curr.empty() || curr.back() <= nums[ind]){
+            curr.push_back(nums[ind]);
+            fn(ind+1, curr, nums, stt);
+            curr.pop_back();
+        }
         fn(ind+1, curr, nums, stt);
     }
     
