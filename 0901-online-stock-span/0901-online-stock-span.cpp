67e1996515367fc4567db8b203f727cd3779818b
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2023-01-24 19:59:09 +0530

    Time: 550 ms (19.92%), Space: 84.2 MB (45.65%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.cpp
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -1,18 +1,16 @@
 class StockSpanner {
 public:
-    
+    // Editorial : https://www.youtube.com/watch?v=slYh0ZNEqSw&t=801s
     stack<pair<int,int>> stt;
     StockSpanner() {
-         
+        // already initialised globally   
     }
-    
     int next(int price) {
         int currSpan = 1;
         while(!stt.empty() && stt.top().first <= price) {
             currSpan += stt.top().second;
             stt.pop();
         }
-        
         stt.push({price, currSpan});
         return currSpan;
     }
