9738cb8bfe89e083c23bac48bb002933192464ee
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2023-01-25 14:18:48 +0530

    Time: 190 ms (34.75%), Space: 86.4 MB (14.59%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -10,30 +10,21 @@
  * };
  */
 class Solution {
-// private:
-//     int goodCnt = 0;
-    
-//     void dfs(TreeNode* root, int currMax){
-//         if(root == NULL) return;
-        
-//         if(root->val >= currMax){
-//             currMax = root->val;
-//             goodCnt++;
-//         }
-//         dfs(root->left, currMax);
-//         dfs(root->right, currMax);
-//     }
-    
-public:
-    int goodNodes(TreeNode* root, int curMax = INT_MIN) {
-        int ans = 0;
-        if(root == NULL) return 0;
-        
-        if(root -> val >= curMax){
-            curMax = root -> val;
+private:
+    int ans = 0;
+    void dfs(TreeNode* root, int maxi){
+        if(root == NULL) return ;
+        // cout << root -> val << " " << maxi << endl;
+        if(root -> val >= maxi) {
             ans++;
         }
-        ans += goodNodes(root -> left, curMax) + goodNodes(root -> right, curMax);
+        maxi = max(maxi, root -> val);
+        dfs(root->left, maxi);
+        dfs(root->right, maxi);
+    }
+public:
+    int goodNodes(TreeNode* root) {
+        dfs(root, -1e6);
         return ans;
     }
 };
