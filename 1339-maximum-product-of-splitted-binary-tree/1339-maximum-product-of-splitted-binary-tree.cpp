3aa1e05af8957cf8cc49f6107281f7301a2ee4a3
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2023-01-25 15:53:57 +0530

    Time: 387 ms (6.19%), Space: 79.3 MB (37.54%) - LeetHub

--- 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
@@ -10,24 +10,27 @@
  * };
  */
 class Solution {
-private:
-    long long ans = 0, fullSum = 0, mod = 1e9 + 7;
+public:
+    long long ans = 0, total = 0;
     int dfs(TreeNode* root){
         if(root == NULL) return 0;
-        
-        // Idea : ans is max of all subTreeSum*(total - subTreeSum)
-        // we did post order because, in that case
-        // leafs are done first and root is added, which is indirectle subtree sum
-        
-        long long currSum = dfs(root -> left) + dfs(root -> right) + root -> val;
-        ans = max(ans, currSum*(fullSum - currSum));
+        int currSum = root -> val;
+        currSum += dfs(root->left) + dfs(root->right);
+        ans = max(ans, currSum*(total - currSum));
         return currSum;
     }
-public:
+    
     int maxProduct(TreeNode* root) {
-        fullSum = dfs(root); // It'll get the sum of toatal tree
-
-        dfs(root); //  in the next traversal, we calculate sum of subrees
-        return ans%mod;
+        queue<TreeNode*> que;
+        que.push(root);
+        while(!que.empty()){
+            TreeNode* it = que.front(); 
+            que.pop();
+            total += it -> val;
+            if(it -> left) que.push(it -> left);
+            if(it -> right) que.push(it -> right);
+        }
+        dfs(root);
+        return ans%1000000007;
     }
 };
