364e1b7a96688202bdf6ae5df809dce39d4f9d09
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2023-01-31 15:02:15 +0530

    Time: 3 ms (78.10%), Space: 11.9 MB (17.76%) - LeetHub

---
+++ 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int fillCups(vector<int>& amount) {
+        priority_queue<int> pque;
+        for(int it : amount) pque.push(it);
+        int ans = 0;
+        while(pque.size() >= 2){
+            int a = pque.top();
+            pque.pop();
+            int b = pque.top();
+            pque.pop();
+            if(a>0) a -= 1;
+            else break;
+            if(b>0) b -= 1;
+            if(a>0) pque.push(a);
+            if(b>0) pque.push(b);
+            ans++;
+        }
+        if(pque.size() == 1) ans += pque.top();
+        return ans;
+    }
+};
