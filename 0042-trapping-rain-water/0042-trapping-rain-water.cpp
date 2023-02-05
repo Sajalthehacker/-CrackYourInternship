038c12e77a9f18ffbed4dcccb558cd27ced5a506
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2023-02-05 19:43:11 +0530

    Time: 19 ms (59.77%), Space: 20 MB (48.34%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -3,18 +3,18 @@ public:
     int trap(vector<int>& height) {
         int n = height.size();
         // we consider how much amount of water every block contributes
-        int ngr[n], ngl[n];
+        int suffMax[n], prefMax[n];
         for(int i=n-1; i>=0; i--){
-            if(i == n-1) ngr[i] = height[i];
-            else ngr[i] = max(ngr[i+1], height[i]);
+            if(i == n-1) suffMax[i] = height[i];
+            else suffMax[i] = max(suffMax[i+1], height[i]);
         }
         for(int i=0; i<n; i++){
-            if(i == 0) ngl[i] = height[0];
-            else ngl[i] = max(ngl[i-1], height[i]);
+            if(i == 0) prefMax[i] = height[0];
+            else prefMax[i] = max(prefMax[i-1], height[i]);
         }
         int total = 0;
         for(int i=0; i<n; i++)
-            total += min(ngl[i], ngr[i])-height[i];
+            total += min(prefMax[i], suffMax[i])-height[i];
         
         return total;
     }
