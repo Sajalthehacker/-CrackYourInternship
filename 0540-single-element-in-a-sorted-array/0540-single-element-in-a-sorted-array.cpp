bf4a780b883a485a1ca9f1f4d004ec02a5236e83
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2023-02-21 20:21:14 +0530

    Time: 36 ms (25.60%), Space: 22.4 MB (16.37%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -2,22 +2,30 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         if(nums.size() == 1) return nums[0];
-        
-        int low = 0, high = nums.size() - 1, mid;
+        int n = nums.size();
+        int low = 0, high = n - 1, mid;
         
         while(low < high){
             mid = low + (high - low) / 2;
-            // number ^ 1 flips the last bit,
-            // if num is odd it becomes odd - 1
-            // if num is even it becomes even + 1 
-            
-            // If this isnt undersytandable , refer previous submissions
-            if(nums[mid] == nums[mid^1]){  
-                low = mid + 1;
+            if(mid%2 == 0){
+                if(mid > 0 && nums[mid] == nums[mid-1]){
+                    high = mid - 1; 
+                }
+                else if(mid < n-1 && nums[mid] == nums[mid+1]){
+                    low = mid + 1;
+                }
+                else return nums[mid];
             }
-            else high = mid;
+            else{
+                if(mid > 0 && nums[mid] == nums[mid-1]){
+                    low = mid + 1; 
+                }
+                else if(mid < n-1 && nums[mid] == nums[mid+1]){
+                    high = mid - 1;
+                }
+                else return nums[mid];
+            }
         }
         return nums[low];
-        
     }
 };
