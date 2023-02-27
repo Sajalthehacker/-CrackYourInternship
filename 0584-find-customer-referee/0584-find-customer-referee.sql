14074aeb8476786622ff091f596aec04103ab223
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2023-02-27 13:10:37 +0530

    Time: 1396 ms (5.02%), Space: 0B (100.00%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,8 +1,9 @@
 # Write your MySQL query statement below
 
 # SELECT name FROM Customer  
-#     WHERE referee_id IS NULL OR referee_id <> 2;  -- Can even use this , below is more efficient
+#     WHERE referee_id IS NULL OR referee_id <> 2;  -- Can even use this
+
+# In SQL : <>  means  not equals to
     
 SELECT name FROM Customer
     WHERE IFNULL(referee_id, 0) <> 2; -- if referee_id is NULL it takes it as 0
-    -- <> : means ot equals to
