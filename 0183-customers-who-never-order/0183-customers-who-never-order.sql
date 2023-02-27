89752d78b686d0da2835347fce57a11623483af1
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2023-02-27 13:18:26 +0530

    Time: 1004 ms (41.25%), Space: 0B (100.00%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -1,5 +1,8 @@
 # Write your MySQL query statement below
 
+# C.name AS Customers -> helps changing the header name in the OUTPUT
 
-SELECT C.name AS Customers FROM Customers AS C LEFT JOIN Orders AS O ON C.id = O.customerId
+SELECT C.name AS Customers 
+    FROM Customers AS C LEFT JOIN Orders AS O 
+    ON C.id = O.customerId
     WHERE O.id IS NULL;
