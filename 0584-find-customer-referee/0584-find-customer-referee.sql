cf8864130c721e855cf96c3f81745120cf262460
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2023-02-27 13:09:48 +0530

    Time: 966 ms (36.56%), Space: 0B (100.00%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,4 +1,8 @@
 # Write your MySQL query statement below
 
-SELECT name FROM Customer 
-    WHERE referee_id IS NULL OR referee_id <> 2; 
+# SELECT name FROM Customer  
+#     WHERE referee_id IS NULL OR referee_id <> 2;  -- Can even use this , below is more efficient
+    
+SELECT name FROM Customer
+    WHERE IFNULL(referee_id, 0) <> 2; -- if referee_id is NULL it takes it as 0
+    -- <> : means ot equals to
