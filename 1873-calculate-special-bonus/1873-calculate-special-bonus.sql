c359defac8c1437edbcf723410572d28e08b14cb
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2023-03-01 11:10:53 +0530

    Time: 1125 ms (50.23%), Space: 0B (100.00%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -1,11 +1,14 @@
 # Write your MySQL query statement below
 
-SELECT employee_id, salary AS bonus FROM Employees 
-    WHERE name NOT LIKE "M%" AND MOD(employee_id, 2) <> 0
+# In this Approach we are doing unin of bonus 100% and bonus 0 tuples
+# SELECT employee_id, salary AS bonus FROM Employees WHERE name NOT LIKE "M%" AND MOD(employee_id, 2) <> 0
+# UNION
+# SELECT employee_id, 0 AS bonus FROM Employees WHERE name LIKE "M%" OR MOD(employee_id, 2) = 0
+# ORDER BY employee_id;
 
-UNION
 
-SELECT employee_id, 0 AS bonus FROM Employees 
-    WHERE name LIKE "M%" OR MOD(employee_id, 2) = 0
-    
-ORDER BY employee_id;
+# Efiicient Apporach
+
+SELECT employee_id, 
+    (salary * MOD(employee_id, 2) * (name NOT LIKE "M%")) AS bonus -- if e_id is even or name is not M% salary is *0;
+FROM Employees ORDER BY employee_id; 
