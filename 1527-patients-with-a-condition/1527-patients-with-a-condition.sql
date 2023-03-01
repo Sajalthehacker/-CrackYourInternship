e9b310699a012a9d1ad2e04408690221fd436ea1
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2023-03-01 12:16:07 +0530

    Time: 1070 ms (5.03%), Space: 0B (100.00%) - LeetHub

--- 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
+++ 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
@@ -1,5 +1,3 @@
 # Write your MySQL query statement below
 
-SELECT * FROM Patients WHERE conditions LIKE "% DIAB1%" 
-    UNION 
-SELECT * FROM Patients WHERE conditions LIKE "DIAB1%";
+SELECT * FROM Patients WHERE conditions LIKE "% DIAB1%" OR conditions LIKE "DIAB1%";
