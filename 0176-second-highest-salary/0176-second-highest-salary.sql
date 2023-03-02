a2458244af00a8f41751d3b53c826f92dd9f2b46
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2023-03-03 00:20:28 +0530

    Time: 406 ms (56.71%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,4 +1,6 @@
 # Write your MySQL query statement below
 
+# https://leetcode.com/problems/second-highest-salary/discuss/1168444/Summary-Five-ways-to-solve-the-top-n-nth-problems
+
 SELECT MAX(salary) as SecondHighestSalary FROM Employee 
     WHERE salary NOT IN (SELECT MAX(salary) FROm Employee);
