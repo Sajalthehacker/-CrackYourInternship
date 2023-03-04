30a6fc893544b3ebbd8a8ec0c7430620160e568a
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2023-03-04 22:22:59 +0530

    Time: 851 ms (61.90%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,4 +1,6 @@
 # Write your MySQL query statement below
 
+# https://leetcode.com/problems/rising-temperature/discuss/1308831/Faster-oror-Easy
+
 SELECT w1.id FROM Weather AS w1, Weather AS w2
 WHERE w1.temperature > w2.temperature AND DATEDIFF(w1.recordDate, w2.recordDate) = 1;
