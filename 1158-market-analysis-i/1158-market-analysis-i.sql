ffab01bf738b7809889f581da3dd7603ce44f389
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2023-03-09 01:41:31 +0530

    Time: 3065 ms (6.02%), Space: 0B (100.00%) - LeetHub

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -1,5 +1,7 @@
 # Write your MySQL query statement below
 
+# https://leetcode.com/problems/market-analysis-i/discuss/2319471/Simple-In-Depth-explanation-of-WHERE-and-IS-NULL!!
+
 
 SELECT 
     u.user_id as buyer_id, 
@@ -7,5 +9,5 @@ SELECT
     count(o.order_id) as 'orders_in_2019'
 FROM users u
 LEFT JOIN Orders o
-ON o.buyer_id=u.user_id AND YEAR(order_date)='2019'
-GROUP BY u.user_id
+ON o.buyer_id=u.user_id AND YEAR(order_date) = 2019
+GROUP BY u.user_id 
