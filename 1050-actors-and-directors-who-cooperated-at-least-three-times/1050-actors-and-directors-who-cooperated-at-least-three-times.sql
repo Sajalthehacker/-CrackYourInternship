bcee3283d4e93e35f0517c7111227b6afb18d917
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2023-03-09 01:08:16 +0530

    Time: 686 ms (50.46%), Space: 0B (100.00%) - LeetHub

--- 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
+++ 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -1,5 +1,9 @@
 # Write your MySQL query statement below
 
+# WHERE cannot be used in the aggregator function
+# So HAVING can be used in place of WHERE
+
+
 SELECT 
     actor_id, 
     director_id
