7a8425c3b37b272477a5ec5e9eb7996ee2d10f44
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2023-03-14 10:56:22 +0530

    Time: 4 ms (51.23%), Space: 6.1 MB (9.67%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int fn(int n, vector<int> &dp){
+        if(n <= 1) return n;
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = fn(n-1, dp) + fn(n-2, dp); 
+    }
+    
+    int fib(int n) {
+        vector<int> dp(n+1, -1);
+        return fn(n, dp);
+    }
+};
