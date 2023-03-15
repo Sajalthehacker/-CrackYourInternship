0d4d0dcd96b7ac7979a013a4a07d4bd5d4946860
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2023-03-15 16:14:34 +0530

    Time: 9 ms (21.62%), Space: 11.4 MB (84.52%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,26 +1,27 @@
 class Solution {
-private:
-    void fn(int index, int n, int open, int close, string &str, vector<string> &ans){
-        if(index == 2*n) {
-            ans.push_back(str);
-            return;
+public:
+    void fn(string &curr, int open, int close, int n, vector<string> &ans){
+        if(close == n){
+            ans.push_back(curr);
         }
+        
         if(open < n){
-            str.push_back('(');
-            fn(index+1, n, open+1, close, str, ans);
-            str.pop_back();
+            curr.push_back('(');
+            fn(curr, open+1, close, n, ans);
+            curr.pop_back();
         }
         if(close < open){
-            str.push_back(')');
-            fn(index+1, n, open, close+1, str, ans);
-            str.pop_back();
+            curr.push_back(')');
+            fn(curr, open, close+1, n, ans);
+            curr.pop_back();
         }
     }
-public:
+    
+    
     vector<string> generateParenthesis(int n) {
         vector<string> ans;
-        string str = "";
-        fn(0, n, 0, 0, str, ans);
+        string curr = "";
+        fn(curr, 0, 0, n, ans);
         return ans;
     }
 };
