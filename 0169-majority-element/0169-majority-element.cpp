26b2d11ec58f7e8fed6a922db9663b40e70f6629
mohitboyidapu <47803821+mohitboyidapu@users.noreply.github.com>
2023-03-17 00:09:25 +0530

    Time: 25 ms (36.78%), Space: 19.5 MB (97.43%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        // https://www.youtube.com/watch?v=AoX3BPWNnoE&list=PLgUwDviBIf0rPG3Ictpu74YWBQ1CaBkm2&index=17
         
+        
+        // Moore's Algo
         int cnt = 0 , ele;
         for(int it : nums){
-            if(cnt == 0) {
+            if(cnt == 0){
                 ele = it;
-                cnt++;
             }
-            else if(it == ele) cnt++;
+            if(it == ele) cnt++;
             else cnt--;
         }
         return ele;
